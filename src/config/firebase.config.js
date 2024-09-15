e0b6086d174980ba1d273f9e33609b68b4233315
JaaniCoder <157714214+JaaniCoder@users.noreply.github.com>
2024-09-15 22:12:03 +0530

    Update firebase.config.js

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCW4xknJBd0rrkci007oyVD_RMRaWEylvg",
-    authDomain: "code-editor-13791.firebaseapp.com",
-    projectId: "code-editor-13791",
-    storageBucket: "code-editor-13791.appspot.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
     messagingSenderId: process.env.MESSAGESENDERID,                  
-    appId: "1:543112819336:web:d612d875a2c84e8dfca53d",
+    appId: process.env.APP_ID,
   };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
